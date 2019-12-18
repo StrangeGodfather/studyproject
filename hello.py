94e9ae56fd36aa1418a570e2d2a99827bc0cd38d
jeremy <jeremy@neweximbills.com>
2019-12-18 17:42:40 +0800

    1218

--- hello.py
+++ hello.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 msg="Hello ,World !  Python3"
 print(msg)
+import math
+math.pi
+print(math.pi)
+import datetime
+datetime.datetime.today()
+print(math.e)
+a,b = 1,2
+if a>b:
+    x = a
+else:
+    x = b
+print(x)
+
+fruits = ['apple','orange','banana']
+a = list(range(1,11,2))
+print(a)
+b = [2**i for i in range(5)]
+print(b)
