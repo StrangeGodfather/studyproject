48dbfce4f3dbf691d5078dc5e80e20ff56a9b4b9
Jeremy Zhang <103275756@qq.com>
2020-03-25 12:44:19 +0800

    20200325

---
+++ nfs-server-setup-200.208.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This script should be executed on Linux Ubuntu Virtual Machine
+
+EXPORT_DIRECTORY=${1:-/export/data}
+DATA_DIRECTORY=${2:-/data}
+CS_SUBNET=${3:-10.39.0.0/16}
+
+echo "Updating packages"
+apt-get -y update
+
+echo "Installing NFS kernel server"
+
+apt-get -y install nfs-kernel-server
+
+echo "Making data directory ${DATA_DIRECTORY}"
+mkdir -p ${DATA_DIRECTORY}
+
+echo "Making new directory to be exported and linked to data directory: ${EXPORT_DIRECTORY}"
+mkdir -p ${EXPORT_DIRECTORY}
+
+echo "Mount binding ${DATA_DIRECTORY} to ${EXPORT_DIRECTORY}"
+mount --bind ${DATA_DIRECTORY} ${EXPORT_DIRECTORY}
+
+echo "Giving 777 permissions to ${EXPORT_DIRECTORY} directory"
+chmod 777 ${EXPORT_DIRECTORY}
+
+parentdir="$(dirname "$EXPORT_DIRECTORY")"
+echo "Giving 777 permissions to parent: ${parentdir} directory"
+chmod 777 $parentdir
+
+echo "Appending bound directories into fstab"
+echo "${DATA_DIRECTORY}    ${EXPORT_DIRECTORY}   none    bind  0  0" >> /etc/fstab
+
+echo "Appending localhost and Kubernetes subnet address ${CS_SUBNET} to exports configuration file"
+echo "/export        ${CS_SUBNET}(rw,async,insecure,fsid=0,crossmnt,no_subtree_check)" >> /etc/exports
+echo "/export        localhost(rw,async,insecure,fsid=0,crossmnt,no_subtree_check)" >> /etc/exports
+
+nohup service nfs-kernel-server restart
